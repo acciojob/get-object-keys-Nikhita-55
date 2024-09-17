c1f5f295cf3123ae36098578ae0ad917ee8f3c1d
NIKHITA PARDHE <153489800+Nikhita-55@users.noreply.github.com>
2024-09-17 19:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Create the student object with a name property
+let student = {
+    name: "John Doe"
+};
+
+// Add the getKeys method to Object.prototype
+Object.prototype.getKeys = function() {
+    // Return an array of keys from the current object
+    return Object.keys(this);
+};
+
+// Test the getKeys method
+console.log(student.getKeys()); // Output: ["name"]
